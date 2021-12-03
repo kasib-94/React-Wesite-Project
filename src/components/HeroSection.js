91db694ae3b6c6afe9202879715f81c933644b51
kasib-94 <jakczarski@gmail.com>
2021-12-03 05:55:43 +0100

    adding github-pages to project to publish project

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,13 +2,13 @@ import React from "react";
 import './HeroSection.css';
 import {Button} from "./Button";
 import '../App.css';
-import video from "../videos/video-1.mp4"
+
 
 
 function HeroSection() {
     return (
         <div className="hero-container">
-            <video  src={video} autoPlay loop muted />
+
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for ?</p>
             <div className="hero-btns">
